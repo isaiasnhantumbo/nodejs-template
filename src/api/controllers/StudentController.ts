16d0511a9b25a5561b83503e1bc379129774d13b
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2022-11-21 01:18:49 +0200

    ✨ added get student by Id route

--- src/api/controllers/StudentController.ts
+++ src/api/controllers/StudentController.ts
@@ -1,7 +1,9 @@
-import { createStudent } from "./../../app/Students/CreateStudent";
-import { container } from "tsyringe";
 import { Router } from "express";
+import { container } from "tsyringe";
+
+import { createStudent } from "./../../app/Students/CreateStudent";
 import { getAllStudent } from "../../app/Students/GetAllStudents";
+import { getStudentById } from "../../app/Students/GetStudentById";
 const studentsRoutes = Router();
 
 studentsRoutes.post("/student", (req, res) => {
@@ -9,6 +11,9 @@ studentsRoutes.post("/student", (req, res) => {
 });
 studentsRoutes.get("/student", (req, res) => {
   getAllStudent.handle(req, res);
+});
+studentsRoutes.get("/student/:id", (req, res) => {
+  getStudentById.handle(req, res);
 });
 
 export { studentsRoutes };
