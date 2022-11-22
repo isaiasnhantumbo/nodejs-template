6fad8c236b65c38b0d67e06512c4c00c6d0886ee
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2022-11-22 23:04:51 +0200

    🔧 updated build scripts

--- src/persistence/data-source.ts
+++ src/persistence/data-source.ts
@@ -1,15 +1,20 @@
 import "dotenv/config";
 import "reflect-metadata";
+
 import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
-    type:'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT as number | undefined,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities:[`../**/domain/*.{ts,js}`],
-    migrations:[`${__dirname}/**/migrations/*.{ts,js}`],
-    logging:true
-})
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT as number | undefined,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [
+    process.env.NODE_ENV === "production"
+      ? `dist/domain/*.{ts,js}`
+      : `src/**/domain/*.{ts,js}`,
+  ],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+  logging: true,
+});
